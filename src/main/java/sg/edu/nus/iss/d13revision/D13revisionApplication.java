7df1b16a9c08a204c384970031d3e8d0c8cc7b8a
Sathya <sathyamelwani@gmail.com>
2022-07-21 09:48:19 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/d13revision/D13revisionApplication.java
@@ -0,0 +1,45 @@
+package sg.edu.nus.iss.d13revision;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.boot.*;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.*;
+@SpringBootApplication
+public class D13revisionApplication {
+
+	public static void main(String[] args) {
+	//	SpringApplication.run(D13revisionApplication.class, args);
+
+	SpringApplication app = new SpringApplication(D13revisionApplication.class)
+
+	String port = "8080";
+	ApplicationArguments cli0pts = new DefaultApplicationArguments(args);
+
+	if (cli0pts.containsOption("port")) {
+		port = cli0pts.getOptionValues("port").get(0);
+	}
+
+	app.setDefaultProperties(
+		Collections.singletonMap("server.port", port));
+		
+	
+	app.run(args);
+
+}
+
+@Bean
+
+public CommonsRequestLoggingFilter log() {
+	CommonsRequestLoggingFilter logger = new CommonsRequestLoggingFilter();
+	logger.setIncludeClientInfo(true);
+	logger.setIncludeQueryString(true);
+	return logger;
+
+}
+}
